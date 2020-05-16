e02401fcf9c82e6c942011a33839abe799469a46
Chris <cesperian@gmail.com>
2020-05-16 15:17:24 +0700

    commit subdirs

---
+++ src/5x5jqpi.js
@@ -0,0 +1,48 @@
+
+(function(){
+
+    let manifest = [];
+
+    $.fn.initUploader = function(o) {
+
+        let files = [];
+        let trackTarget, trackProximate;
+        let $ulObj = this;
+
+        //1000000 =~ 1Mb default
+        let defaults = {
+            destination:null,
+            destinationParams:null,
+            sizeLimit:1,
+            fileLimit:5,
+            options:null,
+            description:false,
+            postFn:$.noop
+        };
+
+        let opts = $.extend({},defaults,o);
+
+        opts.fileSizeLimit *= 1000000; // config param = Mb
+        if(opts.destinationParams)opts.destination = () => {
+            let params = `${opts.destination}?`;
+            params += opts.destinationParams.each((k,v) => `${k}=${v}&`);
+            return params
+        };
+
+        // todo; set up .on for any generated form el validation
+
+        // set up submit/cancel bindings...
+        $(".dDropOptsCont button:contains('Upload')").click(function(){
+            handleFileUpload(files,$ulObj,opts.refreshUrl,opts.destination,true);
+            // add progress bar
+            files = [];
+
+        }).siblings("button:contains('Cancel')").click(function(){
+            // destroy ui els
+            files = [];
+        });
+
+
+    }
+
+}()); //iife
